1de79b156a4399beb0820fcf89f7d4187f1fcf09
GollmerHenrik <henrik@gollmer-ratki.de>
2024-10-15 14:07:50 +0200

    Kiírja

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', 'app()->getLocale())')}}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+    <meta name="csrf-token" content="{{csrf_token()}}">
+ 
+    <title>{{ config('app.name', 'CarLog')}}</title>
+</head>
+<body>
+    <div id="app">
+        <div class="container">
+
+        </div>
+    </div>
+    <main class="py-4">
+        @yield('content')
+    </main>
+    <footer class="text-center">
+        
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MakerController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('makers', [MakerController::class, 'index'])->name('makers');
